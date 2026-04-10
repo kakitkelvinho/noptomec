58e0d734b4c9009c4b2df5b87b3915af51cd274e
Kelvin Aalto <kelvin.ho@aalto.fi>
2026-04-10 17:33:04 +0300

    just in case

--- src/components/capturepicker.tsx
+++ src/components/capturepicker.tsx
@@ -1,6 +1,5 @@
-import React, { ReactElement, useState } from 'react';
-import { CaptureData } from '@/utils/dataconverter';
-
+import React, { ReactElement, useState } from "react";
+import { CaptureData } from "@/utils/dataconverter";
 
 interface captureID {
   key: string;
@@ -19,27 +18,33 @@ interface CapturePickerProps {
   repo: string;
 }
 
-export default function CapturePicker({ availableCaptures, setAvailableTraces, username, repo }: CapturePickerProps): ReactElement {
-
+export default function CapturePicker({
+  availableCaptures,
+  setAvailableTraces,
+  username,
+  repo,
+}: CapturePickerProps): ReactElement {
   const [selectedCaptures, setSelectedCaptures] = useState<captureID[]>([]);
 
   const selectCapture = async (event: React.FormEvent): Promise<void> => {
-    event.preventDefault()
+    event.preventDefault();
     try {
       const traces: CaptureObject[] = [];
       selectedCaptures.forEach(async (capture: captureID): Promise<void> => {
-        const response = await fetch(`https://cdn.jsdelivr.net/gh/${username}/${repo}@main/${capture.folder}/${capture.name}`);
+        const response = await fetch(
+          `https://cdn.jsdelivr.net/gh/${username}/${repo}@main/${capture.folder}/${capture.name}`,
+        );
         const data = await response.json();
         traces.push({
           ...capture,
-          data: data
+          data: data,
         });
         setAvailableTraces(traces);
       });
     } catch (error) {
-      console.log('Error', error);
+      console.log("Error", error);
     }
-  }
+  };
 
   const handleCaptureCheckboxChange = (capture: captureID) => {
     setSelectedCaptures((prev: captureID[]): captureID[] => {
@@ -50,32 +55,42 @@ export default function CapturePicker({ availableCaptures, setAvailableTraces, u
     });
   };
 
-
   return (
     <>
       <form onSubmit={selectCapture}>
-        {availableCaptures.map((capture: captureID, i: number): React.ReactElement => (
-          <div key={i}>
-            <input
-              type="checkbox"
-              id={`${capture.folder}-${capture.name}`}
-              name={`${capture.folder}/${capture.name}`}
-              onChange={() => handleCaptureCheckboxChange(capture)}
-            />
-            <label htmlFor={`${capture.folder}-${capture.name}`}>{`${capture.folder}/${capture.name}`}</label>
-          </div>
-        ))}
+        {availableCaptures.map(
+          (capture: captureID, i: number): React.ReactElement => (
+            <div key={i}>
+              <input
+                type="checkbox"
+                id={`${capture.folder}-${capture.name}`}
+                name={`${capture.folder}/${capture.name}`}
+                onChange={() => handleCaptureCheckboxChange(capture)}
+              />
+              <label
+                htmlFor={`${capture.folder}-${capture.name}`}
+              >{`${capture.folder}/${capture.name}`}</label>
+            </div>
+          ),
+        )}
         <button
           type="submit"
-          className="bg-gray-400 hover:bg-gray-300 mx-4 px-4 py-1 rounded">
+          className="bg-gray-400 hover:bg-gray-300 mx-4 px-4 py-1 rounded"
+        >
           Load data
         </button>
       </form>
       {/* For debugging, to see whether captures are picked */}
       <div>
         <h2 className="bold text-xl underline">Selected traces: </h2>
-        {selectedCaptures.map((capture: captureID, i: number): React.ReactElement => <p key={i}>{capture.folder}/{capture.name}</p>)}
+        {selectedCaptures.map(
+          (capture: captureID, i: number): React.ReactElement => (
+            <p key={i}>
+              {capture.folder}/{capture.name}
+            </p>
+          ),
+        )}
       </div>
     </>
-  )
+  );
 }
